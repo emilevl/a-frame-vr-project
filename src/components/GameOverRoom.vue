3684317b976b9a078f1f40accacd4a194b46d6c6
Emile <emile.vianneyliaud@gmail.com>
2023-03-08 15:55:46 +0100

    Update format document

--- src/components/GameOverRoom.vue
+++ src/components/GameOverRoom.vue
@@ -1,49 +1,53 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 defineProps({
-    rotation: String,
-    position: String,
-    scale: String,
-    allAssetsLoaded: false,
-    safeRoom: Boolean
-  });
+  rotation: String,
+  position: String,
+  scale: String,
+  allAssetsLoaded: false,
+  safeRoom: Boolean,
+});
 
-  let textGameOver = ref("Game Over \n Press the button to be free")
-  let nbClick = 0;
+let textGameOver = ref("Game Over \n Press the button to be free");
+let nbClick = 0;
 
-  function changeText() {
-    if (nbClick == 0 || nbClick == 1) {
-      textGameOver.value = "Kidding, you're doomed"
-    } else if (nbClick == 2) {
-      textGameOver.value = "Stop pressing, it won't change anything..."
-    }else if (nbClick == 3) {
-      textGameOver.value = "Come on, you don't have something to do ? "
-    }else if (nbClick == 4) {
-      textGameOver.value = "Auto explode in 3... 2... 1... "
-      setTimeout(function () { 
-          textGameOver.value = "Lol, I don't have time to make the game explode. " 
-      } , 1500)
-    } else if (nbClick == 5) {
-      textGameOver.value = "No more ideas. But keep on clicking" 
-    }else if (nbClick == 10) {
-      textGameOver.value = "Yeah, continue..." 
-    }else if (nbClick == 15) {
-      window.location.href = 'https://www.youtube.com/watch?v=rSimk1i72J8'
-    }
-    nbClick++
-    setTimeout(function () { 
-        document.querySelector("#game-over-button").emit("unclick");
-      } , 600)
+function changeText() {
+  if (nbClick == 0 || nbClick == 1) {
+    textGameOver.value = "Kidding, you're doomed";
+  } else if (nbClick == 2) {
+    textGameOver.value = "Stop pressing, it won't change anything...";
+  } else if (nbClick == 3) {
+    textGameOver.value = "Come on, you don't have something to do ? ";
+  } else if (nbClick == 4) {
+    textGameOver.value = "Auto explode in 3... 2... 1... ";
+    setTimeout(function () {
+      textGameOver.value = "Lol, I don't have time to make the game explode. ";
+    }, 1500);
+  } else if (nbClick == 5) {
+    textGameOver.value = "No more ideas. But keep on clicking";
+  } else if (nbClick == 10) {
+    textGameOver.value = "Yeah, continue...";
+  } else if (nbClick == 15) {
+    window.location.href = "https://www.youtube.com/watch?v=rSimk1i72J8";
   }
+  nbClick++;
+  setTimeout(function () {
+    document.querySelector("#game-over-button").emit("unclick");
+  }, 600);
+}
 
 const allAssetsLoaded = ref(false);
 </script>
 
 <template>
-
-  <a-entity :position="position" :rotation="rotation" :scale="scale" data-role="room" :data-safe-room="safeRoom">
-    
+  <a-entity
+    :position="position"
+    :rotation="rotation"
+    :scale="scale"
+    data-role="room"
+    :data-safe-room="safeRoom"
+  >
     <a-entity
       geometry="primitive: plane; height: 3; width: 2"
       position="0 0.01 -0.65"
@@ -52,44 +56,42 @@ const allAssetsLoaded = ref(false);
       visible="true"
       material="color: #e2e2e2"
     ></a-entity>
+    <a-entity position="0 0 -2">
       <a-entity
-      position="0 0 -2">
-        <a-entity 
         :text="`value: ${textGameOver}; 
           align: center;
           width: 2.5;`"
         position="0 1.75 -1"
         rotation="0 0 0"
-        ></a-entity>
-      
-        <a-box
+      ></a-entity>
+
+      <a-box
         width="0.25"
         height="1.2"
         depth="0.25"
         position="0 0.6 0"
         material="color: #4a4a4a"
-        > 
-        </a-box>
-        <a-cylinder
-          clickable
-          id="game-over-button"
-          @click="changeText()"
-          material="color: crimson"
-          height="0.05" 
-          radius="0.05"
-          rotation="0 0 0"
-          position="0 1.21 0"
-          animation__onclick="property:position; to:0 1.198 0; startEvents:click; dur: 400;"
-          animation__onunclick="property:position; to:0 1.21 0; dur: 400; startEvents:unclick;"
-          ></a-cylinder>
-      </a-entity>
-      
+      >
+      </a-box>
+      <a-cylinder
+        clickable
+        id="game-over-button"
+        @click="changeText()"
+        material="color: crimson"
+        height="0.05"
+        radius="0.05"
+        rotation="0 0 0"
+        position="0 1.21 0"
+        animation__onclick="property:position; to:0 1.198 0; startEvents:click; dur: 400;"
+        animation__onunclick="property:position; to:0 1.21 0; dur: 400; startEvents:unclick;"
+      ></a-cylinder>
+    </a-entity>
+
     <a-sphere
-    position="0 1.5 0"
+      position="0 1.5 0"
       radius="7"
       material="side: double; color: black"
     >
-
     </a-sphere>
   </a-entity>
 </template>
--- src/components/Room.vue
+++ src/components/Room.vue
@@ -1,31 +1,37 @@
 <script setup>
-import { ref } from 'vue';
-import ViewDoor from './ViewDoor.vue'
-import WallHole from './WallHole.vue';
+import { ref } from "vue";
+import ViewDoor from "./ViewDoor.vue";
+import WallHole from "./WallHole.vue";
 
 defineProps({
-    rotation: String,
-    position: String,
-    scale: String,
-    allAssetsLoaded: false,
-    safeRoom: Boolean
-  });
+  rotation: String,
+  position: String,
+  scale: String,
+  allAssetsLoaded: false,
+  safeRoom: Boolean,
+});
 
 const allAssetsLoaded = ref(false);
 </script>
 
 <template>
-  
   <a-assets @loaded="allAssetsLoaded = true">
-      <!--
+    <!--
         Title: VR Gallery
         Model source: https://sketchfab.com/3d-models/vr-gallery-1ac32ed62fdf424498acc146fad31f7e
         Model author: https://sketchfab.com/mvrc.art (Maxim Mavrichev)
         Model license: CC BY 4.0 ( https://creativecommons.org/licenses/by/4.0/ )
       -->
-    </a-assets>
+  </a-assets>
 
-  <a-entity :position="position" :rotation="rotation" :scale="scale" data-role="room" :data-safe-room="safeRoom" data-visited="false">
+  <a-entity
+    :position="position"
+    :rotation="rotation"
+    :scale="scale"
+    data-role="room"
+    :data-safe-room="safeRoom"
+    data-visited="false"
+  >
     <!-- <a-entity light="type: directional; color: #FFF; intensity: 0.7; " rotation="0 90 0" position="0 1.5 1"></a-entity> -->
     <a-entity
       geometry="primitive: plane; height: 3; width: 3"
@@ -67,11 +73,11 @@ const allAssetsLoaded = ref(false);
       visible="true"
     ></a-entity>
 
-    <a-sphere 
-    material="color: #F7F180" 
-    radius="0.1"
-    position="0 3.03 0"
-    scale="2.2 1 2.2"
+    <a-sphere
+      material="color: #F7F180"
+      radius="0.1"
+      position="0 3.03 0"
+      scale="2.2 1 2.2"
     >
     </a-sphere>
     <a-entity
@@ -89,9 +95,17 @@ const allAssetsLoaded = ref(false);
       gltf-model="#poster-wall"
       position="1.492 1.75 -01"
       rotation="0 0 0"
-      scale="0.45 0.45 0.45"  
+      scale="0.45 0.45 0.45"
     ></a-entity>
-    <ViewDoor rotation="0 -90 0" position="1.5 0 0" :all-assets-loaded="allAssetsLoaded"></ViewDoor>
-    <ViewDoor rotation="0 180 0" position="0 0 1.5" :all-assets-loaded="allAssetsLoaded"></ViewDoor>
+    <ViewDoor
+      rotation="0 -90 0"
+      position="1.5 0 0"
+      :all-assets-loaded="allAssetsLoaded"
+    ></ViewDoor>
+    <ViewDoor
+      rotation="0 180 0"
+      position="0 0 1.5"
+      :all-assets-loaded="allAssetsLoaded"
+    ></ViewDoor>
   </a-entity>
 </template>
